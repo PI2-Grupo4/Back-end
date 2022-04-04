1c73d44143b5c69102cf88945d15eb61c00c91cb
Rafael Ribeiro <rafaelflarrn@gmail.com>
2022-04-03 23:17:33 -0300

    Initial commit with Spring Boot, JPA and MySQL

---
+++ back-end-solar-clean/src/test/java/com/pi2/backendsolarclean/BackEndSolarCleanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pi2.backendsolarclean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndSolarCleanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
